2cbe6bba8955f7a1447e166db39af44701f76cf1
Gopakumar R <Gopakumar.R@live.com>
2022-09-17 13:55:20 +1000

    added menu and certifiate

--- MalayalamConverter.UI/Views/UnicodeConverter.xaml.cs
+++ MalayalamConverter.UI/Views/UnicodeConverter.xaml.cs
@@ -11,12 +11,53 @@ public partial class UnicodeConverter : ContentPage
         InitializeComponent();
 
         var viewModel = new Malayalam();
-        viewModel.Fonts = MalayalamFonts.GetAllFonts().OrderBy(r=>r).Select(q => new MalayalamFont() { Name = q }).ToArray();
+        viewModel.Fonts = MalayalamFonts.GetAllFonts().OrderBy(r => r).Select(q => new MalayalamFont() { Name = q }).ToArray();
         this.BindingContext = viewModel;
 
     }
 
-    private void btnConvert_Clicked(object sender, EventArgs e)
+    private async void mnuLoadFile_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            var customFileType = new FilePickerFileType(
+                new Dictionary<DevicePlatform, IEnumerable<string>>
+                {
+                    { DevicePlatform.WinUI, new[] { ".txt", ".doc" } },
+                 });
+
+            PickOptions options = new()
+            {
+                PickerTitle = "Please select a manayalam ascii ",
+                FileTypes = customFileType,
+            };
+
+
+            var result = await FilePicker.Default.PickAsync(options);
+            if (result != null)
+            {
+                if (result.FileName.EndsWith("txt", StringComparison.OrdinalIgnoreCase) ||
+                    result.FileName.EndsWith("doc", StringComparison.OrdinalIgnoreCase))
+                {
+                    using var stream = await result.OpenReadAsync();
+                    using var streamReader = new StreamReader(stream);
+                    txtSourceText.Text = await streamReader.ReadToEndAsync();
+                }
+            }
+
+        }
+        catch (Exception ex)
+        {
+            // The user canceled or something went wrong
+        }
+    }
+
+    private void mnuExit_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void mnuConvert_Clicked(object sender, EventArgs e)
     {
         var sourceText = txtSourceText.Text;
         var font = (lstFont.SelectedItem as MalayalamFont).Name;
@@ -25,8 +66,8 @@ public partial class UnicodeConverter : ContentPage
 
         var convertedText = MalayalamConverter.Core.Converter.ConvertAsciiToMalayalamUnicode(sourceText, mapping);
 
-        txtDesinationText.Text= convertedText;
-
-
+        txtDesinationText.Text = convertedText;
     }
+
+    
 }
